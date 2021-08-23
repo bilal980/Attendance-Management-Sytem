04b92c39eea4ff2e8f741d4a8cdd6d4be1f7054d
bilal980 <rajabilalkhan980@gmail.com>
2021-08-23 09:55:10 +0500

    admin panel config

--- AMS/urls.py
+++ AMS/urls.py
@@ -4,6 +4,11 @@ from django.urls import path
 from django.urls.conf import include
 from . import views
 
+admin.site.site_header = 'Attendance Management System'
+admin.site.site_title = 'Attendance Management System'
+admin.site.index_title = "Welcome to Attendance Management System Admin Panel"
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.signin, name='signin'),
--- adm/urls.py
+++ adm/urls.py
@@ -1,6 +1,7 @@
-from typing import Pattern
+
 from django.urls import path
 from adm import views
+
 urlpatterns = [
     path('',views.admin_home,name='admin_home'),
     path('total/students',views.total_student,name='total_student'),
